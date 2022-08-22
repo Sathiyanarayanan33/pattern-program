28cab469588f9c618ed661296e067cb5d5cf4f1a
Sathiyanarayanan33 <111435779+Sathiyanarayanan33@users.noreply.github.com>
2022-08-22 15:05:01 +0530

    Add files via upload
    use the while loop condition for this program

---
+++ Leep.java
@@ -0,0 +1,37 @@
+class Leep{  //4 row,4 colum
+public static void main(String ar [])
+ {
+  int row=1;
+   while(row<=3){
+
+  int col=1;
+   while(col<=4){
+     
+  if(col==1||col==4){
+   System.out.print(1);}
+   else{
+     System.out.print(0);}
+  col++;
+   }
+   System.out.println();
+    row++;      
+  }
+ } //main
+} //class
+//EX OP
+//1001
+//1001
+//1001
+//1001
+
+
+
+
+
+
+
+
+
+
+
+
